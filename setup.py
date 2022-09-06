dc45cd06ea11a30102b25443da96fb5eae5f28f3
Ryan Hansohn <hansohn@users.noreply.github.com>
2022-09-05 23:42:03 -0700

    simplify setup

--- setup.py
+++ setup.py
@@ -4,20 +4,8 @@
 from setuptools import setup
 
 
-def do_setup():
-    setup_kwargs = {}
-    setup(
-        **setup_kwargs,
-        use_scm_version = {
-            "write_to": "tfrelease/version.py",
-            "write_to_template": "__version__ = \"{version}\"",
-        }
-    )
-
-
-def main():
-    do_setup()
-
-
-if __name__ == '__main__':
-    main()
+setup(
+    use_scm_version = {
+        "write_to": "tfrelease/version.py",
+    }
+)
